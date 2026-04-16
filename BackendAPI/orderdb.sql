45d25852b58e8aa4edcac5a9737d0acb0cea2431
marias667 <mayras1917@gmail.com>
2026-04-16 08:12:07 -0500

    Create orderdb.sql

---
+++ BackendAPI/orderdb.sql
@@ -0,0 +1,11 @@
+/*Author: Mayra Arias CSCI 467*/
+/*Order Database*/
+
+CREATE TABLE orderdb (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
+	quotes_id INT,
+	created DATETIME CURRENT_TIMESTAMP,
+	commission INT NOT NULL,
+	quote_amount DECIMAL(11, 2),
+	PRIMARY KEY(id)
+);
