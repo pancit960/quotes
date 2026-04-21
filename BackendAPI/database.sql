20cbfb4e566cbd0249898f79d2a1e38d075bb3f7
Leyla Kucukkaya <lkucukkaya826@gmail.com>
2026-04-21 15:07:43 -0500

    fix: move associate_data table import into own file and makes the id auto increment

--- BackendAPI/database.sql
+++ BackendAPI/database.sql
@@ -31,10 +31,4 @@ CREATE TABLE associate_data (
   address VARCHAR(255)
 );
 
-INSERT INTO associate_data
-VALUES 
-(10000001, 'Apple Banana', 'applePass123', .01, '123 Hashbrown Time'),
-(10000002, 'Orange Grape', '0)0)0)0)', 12.34, '123455677 ABC'),
-(10000003, 'Straw Berry', 'password', 1234.56, '456 Tree'),
-(10000004, 'Potato Tomato', 'QcU74nc)%f', 0, '1Hhhhhhhh'),
-(10000005, 'Apple Sauce', 'passwword', 11111.11, 'NIU');
+ALTER TABLE associate_data AUTO_INCREMENT = 10000001;
--- sql_import/import.sql
+++ sql_import/import.sql
@@ -1,9 +1,9 @@
 USE csci467proj;
 
-INSERT INTO associate_data
+INSERT INTO associate_data (name, user_pass, commission, address)
 VALUES 
-(10000001, 'Apple Banana', 'applePass123', .01, '123 Hashbrown Time'),
-(10000002, 'Orange Grape', '0)0)0)0)', 12.34, '123455677 ABC'),
-(10000003, 'Straw Berry', 'password', 1234.56, '456 Tree'),
-(10000004, 'Potato Tomato', 'QcU74nc)%f', 0, '1Hhhhhhhh'),
-(10000005, 'Apple Sauce', 'passwword', 11111.11, 'NIU');
+('Apple Banana', 'applePass123', .01, '123 Hashbrown Time'),
+('Orange Grape', '0)0)0)0)', 12.34, '123455677 ABC'),
+('Straw Berry', 'password', 1234.56, '456 Tree'),
+('Potato Tomato', 'QcU74nc)%f', 0, '1Hhhhhhhh'),
+('Apple Sauce', 'passwword', 11111.11, 'NIU');
