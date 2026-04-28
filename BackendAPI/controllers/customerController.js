96e48df3233bec883424aee1a83e927effa41166
Leyla Kucukkaya <lkucukkaya826@gmail.com>
2026-04-28 13:36:22 -0500

    fix: customer list page fail to render issue

--- BackendAPI/controllers/customerController.js
+++ BackendAPI/controllers/customerController.js
@@ -1,55 +1,69 @@
 //Author: Riff Talsma
 //This will provide read only access to our legacy database provided in the project
 //Need id, name, city, street, contact info
-const customerModel = require('../models/customerModel');
- 
+const customerModel = require("../models/customerModel");
+
 //return all customers
 async function getAll(req, res) {
-    try {
-        const customers = await customerModel.findAll();
-        res.json({success: true, data: customers});
-    }
+  try {
+    const customers = await customerModel.findAll();
+    res.render("customers", { customers });
+  } catch (err) {
     //debug for group
-    catch(err){
-        console.error('[customerController.getAll]', err);
-        res.status(500).json({success: false, message: 'Could not retrieve all customers from external legacy database. FIX THIS!'});
-    }
+    console.error("[customerController.getAll]", err);
+    res.status(500).json({
+      success: false,
+      message:
+        "Could not retrieve all customers from external legacy database. FIX THIS!",
+    });
+  }
 }
- 
+
 //return single customer by id
 async function getSingle(req, res) {
-    try {
-        const customer = await customerModel.findById(req.params.id);
-        if(!customer) {
-            //throw error 404 cus we aint got the customer
-            return res.status(404).json({success: false, message: 'Could not find customer.'});
-        }
+  try {
+    const customer = await customerModel.findById(req.params.id);
+    if (!customer) {
+      //throw error 404 cus we aint got the customer
+      return res
+        .status(404)
+        .json({ success: false, message: "Could not find customer." });
     }
-    catch(err) {
-        console.error('[customerController.getSingle]', err);
-        res.status(500).json({success: false, nessage: 'Function failed to search a customer.'});
-    }
+    return res.json({
+      success: true,
+      data: customer,
+    });
+  } catch (err) {
+    console.error("[customerController.getSingle]", err);
+    res.status(500).json({
+      success: false,
+      nessage: "Function failed to search a customer.",
+    });
+  }
 }
- 
- 
+
 //search for a customer by name, city or contact info
 async function search(req, res) {
-    try {
-        const term = req.query.q || '';
-        if(!term.trim()) {
-            return res.status(400).json({success: false, message: 'Search term (q) is required.'});
-        }
-        const customers = await customerModel.search(term);
-        res.json({success: true, data: customers});
+  try {
+    const term = req.query.q || "";
+    if (!term.trim()) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Search term (q) is required." });
     }
-    catch(err) {
-        console.error('[customerController.search]', err);
-        res.status(500).json({success: false, message: 'Function failed to search a customer'});
-    }
+    const customers = await customerModel.search(term);
+    res.json({ success: true, data: customers });
+  } catch (err) {
+    console.error("[customerController.search]", err);
+    res.status(500).json({
+      success: false,
+      message: "Function failed to search a customer",
+    });
+  }
 }
- 
-module.exports = {getAll, getSingle, search};
- 
+
+module.exports = { getAll, getSingle, search };
+
 //clean up this code and make sure we use the stuff in our database
 /*
 module.exports = {
@@ -68,4 +82,3 @@ module.exports = {
     }
  
 };*/
- 
